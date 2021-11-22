2b1182bf378e3439d8e05e829ebd0d09f907c43a
Victoria Miranda <vitorianevesmiranda@gmail.com>
2021-11-22 18:39:43 -0300

    firstday

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,32 @@
+import Navbar from "./Navbar"
+import axios from 'axios'
+import {useEffect, useState} from 'react'
+
 function AlbumList () {
+    const [albuns, setAlbuns] = useState([{_id: '619be1cb21b7950017ceeb2b', 'pearl jam': 'lightning bolt'}])
+    let res = [{_id: '619be1cb21b7950017ceeb2b', 'pearl jam': 'lightning bolt'}];
+    // GET em "https://ironrest.herokuapp.com/albuns"
+    useEffect (() => {
+        axios.get("https://ironrest.herokuapp.com/albuns")        
+        .then ((response) => {
+            res = response.data;
+            
+            setAlbuns(response.data)
+            
+        }
+        )
+        .then (() => {
+            console.log(res)
+        })
+    }
+    , [])
+    console.log(res)
     return(
-        <h1>This is the AlbumList</h1>
+        <div>
+            <Navbar />
+            <h1>{albuns[0]["pearl jam"]}</h1>
+            <h1>{res[0]["pearl jam"]}</h1>
+        </div>
     )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,15 @@
+import {Link} from "react-router-dom"
+
 function Navbar() {
     return (
-        <p> I'm a Navbar </p>
+        <div className = "bg-secondary row">
+            <div className = "col">
+                <Link className = "m-2" to = "/">nome</Link>
+            </div>
+            <div className = "col">
+                <Link to = "/albuns">Album List</Link>
+            </div>
+        </div>
     )
 }
 
