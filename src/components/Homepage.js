ad5a6058f057a4c47a00dd04919563515bd2399e
lucas552 <lgstehling@gmail.com>
2021-11-23 10:09:30 -0300

    Not many changes

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,4 +1,15 @@
+import axios from "axios"
+
 function HomePage () {
+    axios.post("https://ironrest.herokuapp.com/reviews", "Só para mostrar como whatever")
+    .then(response => {
+        console.log(response)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+
+
     return (
         <h1>test</h1>
     )
