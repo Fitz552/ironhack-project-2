9cf0b73c8516d67bdeb44b7d1eeadc2444ccb30c
lucas552 <lgstehling@gmail.com>
2021-11-25 15:14:38 -0300

    Adjusted homepage and build for deploy

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route exact path = "/" element={<HomePage/>}/>
         <Route exact path = "/albuns" element={<AlbumList/>}/>
-        <Route exact path = "/albuns/:id" element={<AlbumPage/>}/>
+        <Route exact path = "/albuns/:id" element={<AlbumPageBackup/>}/>
         <Route exact path = "/delete" element={<DeleteCollection/>}/>
         <Route exact path = "/populate" element={<PopulateCollection/>}/>
         <Route exact path = "/remove" element={<RemoveDuplicates/>}/>
